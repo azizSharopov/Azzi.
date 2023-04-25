02d9a32776eaff23d291b1c03cc2be49167fb121
azizSharopov <sharopovaziz23@gmail.com>
2023-04-25 21:47:22 +0900

    feat: memberControllerlarni qurish

--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
 
-router.get("/", function (req, res) {
-  res.send("home sahifasidasiz");
-});
+// memberga daxldor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+// bowqa routerlar
 router.get("/menu", (req, res) => {
   res.send("menu sahifasidasiz");
 });
